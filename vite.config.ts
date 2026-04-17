17d0233d3b170ec40d3fd1e25e36c85f2a3dc71a
czorr <lc@luiszorrilla.com>
2026-04-17 01:57:10 -0600

    vercel deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // Demo/landing build.
 export default defineConfig({
 	plugins: [react()],
+	publicDir: 'src/public',
 	build: {
 		outDir: 'dist/demo',
 		emptyOutDir: true
